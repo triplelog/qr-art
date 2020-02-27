b418cf8e024693f3478c24646ddebee3c97d4b37
triplelog <robmwilc@gmail.com>
2020-02-27 12:34:18 -0500

    my first

--- src/Lapis.QRCode.Imaging.Drawing/GraphicsDrawer.cs
+++ src/Lapis.QRCode.Imaging.Drawing/GraphicsDrawer.cs
@@ -25,7 +25,7 @@ namespace Lapis.QRCode.Imaging.Drawing
             {
                 graph.Clear(ColorHelper.FromIntRgb24(Background));
                 //var foreBrush = new SolidBrush(ColorHelper.FromIntRgb24(Foreground));
-                var foreBrush = new SolidBrush(ColorHelper.FromIntRgb24(555555));
+                var foreBrush = new SolidBrush(Color.FromArgb(255,0,0));
                 for (var r = 0; r < rowCount; r += 1)
                 {
                     for (var c = 0; c < columnCount; c += 1)
