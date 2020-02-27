aca67514642b1ab04080959853702237c7aad0b9
triplelog <robmwilc@gmail.com>
2020-02-27 13:00:11 -0500

    my first

--- src/Lapis.QRCode.Imaging/Rgb24BitmapDrawer.cs
+++ src/Lapis.QRCode.Imaging/Rgb24BitmapDrawer.cs
@@ -9,7 +9,7 @@ namespace Lapis.QRCode.Imaging
 {
     public class Rgb24BitmapDrawer : BitMatrixDrawerBase
     {
-        public override IImage Draw(BitMatrix bitMatrix)
+        public override IImage Draw(BitMatrix bitMatrix, BitMatrix colorMatrix)
         {
             if (bitMatrix == null)
                 throw new ArgumentNullException(nameof(bitMatrix));
