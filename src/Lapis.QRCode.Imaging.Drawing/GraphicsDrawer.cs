3fbd15bd90d256629b20cc303ebf002962e2804a
triplelog <robmwilc@gmail.com>
2020-02-27 23:38:40 -0500

    my first

--- src/Lapis.QRCode.Imaging.Drawing/GraphicsDrawer.cs
+++ src/Lapis.QRCode.Imaging.Drawing/GraphicsDrawer.cs
@@ -51,7 +51,7 @@ namespace Lapis.QRCode.Imaging.Drawing
 									Color myColor = Color.FromArgb(re,gr,bl);
 									if ( r<55 && r>50 && c <55 && c>50){
 										double h; double s; double l;
-										var hsl = RgbToHls(re,gr,bl,h,l,s);
+										var hsl = RgbToHls(re,gr,bl,out h,out l,out s);
 									}
 									var foreBrushCustom = new SolidBrush(myColor);
 									graph.FillRectangle(foreBrushCustom, x+cmj, y+cmi, 1,1);
