06b9fdb336d121f29744613bb26b59f71a4d7cc5
triplelog <robmwilc@gmail.com>
2020-02-27 21:50:09 -0500

    my first

--- src/Lapis.QRCode.Art/QRArtCreator.cs
+++ src/Lapis.QRCode.Art/QRArtCreator.cs
@@ -53,7 +53,7 @@ namespace Lapis.QRCode.Art
             if (image != null)
             {
                 int moduleCount = bitMatrix.Size;
-                image.Save("static/newbmp2.jpg", Lapis.QRCode.Imaging.ImageFormat.Jpeg);
+                image.Save("static/newbmp2.jpg", ImageFormat.Jpeg);
                 var imgBitMatrix = Binarizer.Binarize(image, moduleCount * 3, moduleCount * 3);
                 var imgColorMatrix = Colorizer.Colorize(image, moduleCount * 3, moduleCount * 3);
                 //var imgColorMatrix = new ColorSquare(moduleCount * 3);
