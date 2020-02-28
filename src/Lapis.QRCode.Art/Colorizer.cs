8a6f663207477ababf235510379f0ac9d52f32e1
triplelog <robmwilc@gmail.com>
2020-02-27 21:22:25 -0500

    my first

--- src/Lapis.QRCode.Art/Colorizer.cs
+++ src/Lapis.QRCode.Art/Colorizer.cs
@@ -44,7 +44,7 @@ namespace Lapis.QRCode.Art
             Console.WriteLine(width);
             int[,] rgb24s = new int[rowCount, columnCount];
             //for (int i = 0; i < columnCount; i++)
-            for (int i = 50; i < 100; i++)
+            for (int i = 60; i < 90; i++)
             {
                 //for (int j = 0; j < rowCount; j++)
                 for (int j = 0; j < 10; j++)
@@ -52,7 +52,7 @@ namespace Lapis.QRCode.Art
                     int x = Convert.ToInt32(i);
                     int y = Convert.ToInt32(j);
                     int color = bitmap.GetPixel(x, y);
-                    Console.WriteLine(j);
+                    Console.WriteLine(i);
                     Console.WriteLine(color);
                     rgb24s[j, i] = color;
                 }
