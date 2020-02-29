5f2c942b5c5891079ce5174b7dfda23e64829f50
triplelog <robmwilc@gmail.com>
2020-02-28 21:15:13 -0500

    my first

--- src/Lapis.QRCode.Art/QRAnimationCreator.cs
+++ src/Lapis.QRCode.Art/QRAnimationCreator.cs
@@ -22,7 +22,7 @@ namespace Lapis.QRCode.Art
 
         public Func<IReadOnlyList<IRgb24BitmapFrame>, IRgb24BitmapBase> FrameMerger { get; }
 
-        public override IImage Create(string data, IRgb24BitmapBase image)
+        public override IImage Create(string data, IRgb24BitmapBase image, IRgb24BitmapBase imageText)
         {
             var bitmap = image as IRgb24Bitmap;
             if (bitmap?.FrameCount > 1)
