ce7e4a781be7cd9bd3890683551bee88d69c29d4
triplelog <robmwilc@gmail.com>
2020-02-28 20:09:23 -0500

    my first

--- src/Lapis.QRCode.Art/QRArtCreator.cs
+++ src/Lapis.QRCode.Art/QRArtCreator.cs
@@ -104,8 +104,8 @@ namespace Lapis.QRCode.Art
 									}
 								}
 							}
-							if (minDist < 51){
-								tripMatrix[i,ii] = -1*(70-minDist)/20;
+							if (minDist < 20){
+								tripMatrix[i,ii] = -3;
 							}
                 		}
                 	}
