164fe7048af62951e9119a5ad468db7fcbc843e1
triplelog <robmwilc@gmail.com>
2020-03-29 14:35:49 -0400

    new

--- src/Lapis.QRCode.Art/QRArtCreator.cs
+++ src/Lapis.QRCode.Art/QRArtCreator.cs
@@ -61,6 +61,15 @@ namespace Lapis.QRCode.Art
         	Stopwatch stopWatch = new Stopwatch();
         	stopWatch.Start();
             var bitMatrix = QRCodeEncoder.Build(data);
+            stopWatch.Stop();
+				// Get the elapsed time as a TimeSpan value.
+				TimeSpan ts = stopWatch.Elapsed;
+
+				// Format and display the TimeSpan value.
+				string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+					ts.Hours, ts.Minutes, ts.Seconds,
+					ts.Milliseconds / 10);
+				Console.WriteLine("RunTime " + elapsedTime);
             if (image != null && 2 == 3) //qr code
             {
                 int moduleCount = bitMatrix.Size;
@@ -152,15 +161,7 @@ namespace Lapis.QRCode.Art
                 	}
                 }
                 
-                stopWatch.Stop();
-				// Get the elapsed time as a TimeSpan value.
-				TimeSpan ts = stopWatch.Elapsed;
-
-				// Format and display the TimeSpan value.
-				string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
-					ts.Hours, ts.Minutes, ts.Seconds,
-					ts.Milliseconds / 10);
-				Console.WriteLine("RunTime " + elapsedTime);
+                
                 
                 return TripMatrixDrawer.Draw(tripMatrix, imgColorMatrix);
             }
