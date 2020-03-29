5d1b2faad3dc07a63fc7991061e05286afc85eb6
triplelog <robmwilc@gmail.com>
2020-03-29 13:27:30 -0400

    new

--- src/Lapis.QRCode.Art/QRArtCreator.cs
+++ src/Lapis.QRCode.Art/QRArtCreator.cs
@@ -1,6 +1,7 @@
 using Lapis.QRCode.Encoding;
 using Lapis.QRCode.Imaging;
 using System;
+using System.Diagnostics;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -69,6 +70,8 @@ namespace Lapis.QRCode.Art
             }
             else if (image != null)
             {
+            	Stopwatch stopWatch = new Stopwatch();
+        		stopWatch.Start();
                 int moduleCount = bitMatrix.Size;
                 //var imgBitMatrix = Binarizer.Binarize(image, moduleCount * 3, moduleCount * 3);
                 var imgColorMatrix = Colorizer.Colorize(image, moduleCount * 3, moduleCount * 3);
@@ -114,7 +117,15 @@ namespace Lapis.QRCode.Art
                 		}
                 	}
                 }
-                
+                stopWatch.Stop();
+				// Get the elapsed time as a TimeSpan value.
+				TimeSpan ts = stopWatch.Elapsed;
+
+				// Format and display the TimeSpan value.
+				string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+					ts.Hours, ts.Minutes, ts.Seconds,
+					ts.Milliseconds / 10);
+				Console.WriteLine("RunTime " + elapsedTime);
                 return TripMatrixDrawer.Draw(tripMatrix, imgColorMatrix);
             }
             else {
