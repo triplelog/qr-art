5642bee91b65686106aa5a1842561b70026e0e4a
triplelog <robmwilc@gmail.com>
2020-03-29 14:38:26 -0400

    new

--- src/Lapis.QRCode.Art/QRArtCreator.cs
+++ src/Lapis.QRCode.Art/QRArtCreator.cs
@@ -82,8 +82,7 @@ namespace Lapis.QRCode.Art
                 
                 
 				
-				Stopwatch stopWatch = new Stopwatch();
-        		stopWatch.Start();
+				
         		
                 for (var i=0;i<500;i++){
                 	for (var ii=0;ii<500;ii++){
@@ -91,16 +90,10 @@ namespace Lapis.QRCode.Art
                 	}
                 }
                 
-                stopWatch.Stop();
-				// Get the elapsed time as a TimeSpan value.
-				TimeSpan ts = stopWatch.Elapsed;
-
-				// Format and display the TimeSpan value.
-				string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
-					ts.Hours, ts.Minutes, ts.Seconds,
-					ts.Milliseconds / 10);
-				Console.WriteLine("RunTime " + elapsedTime);
+               
 				
+				Stopwatch stopWatch = new Stopwatch();
+        		stopWatch.Start();
 				
                 for (var i=5;i<495;i++){
                 	for (var ii=5;ii<495;ii++){
@@ -115,6 +108,16 @@ namespace Lapis.QRCode.Art
                 		}
                 	}
                 }
+				
+				stopWatch.Stop();
+				// Get the elapsed time as a TimeSpan value.
+				TimeSpan ts = stopWatch.Elapsed;
+
+				// Format and display the TimeSpan value.
+				string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+					ts.Hours, ts.Minutes, ts.Seconds,
+					ts.Milliseconds / 10);
+				Console.WriteLine("RunTime " + elapsedTime);
 				
 				
         		
